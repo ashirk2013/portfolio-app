036fea170b6d382bed9a50f334fd529ad75db8b0
Aaron Shirk <aaron.shirk@northwindgrp.com>
2022-04-05 16:38:52 -0400

    start building portfolio

--- webui/src/index.js
+++ webui/src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// Create Import File
+import "./index.scss";
+import PageScrollTop from "./component/PageScrollTop";
+import PortfolioHome from "./home/PortfolioHome";
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Root extends Component {
+  render() {
+    return (
+      <BrowserRouter basename={"/"}>
+        <PageScrollTop>
+          <Switch>
+            <Route
+              exact
+              path={`${process.env.PUBLIC_URL}/portfolio-home`}
+              component={PortfolioHome}
+            />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            <Route
+              path={`${process.env.PUBLIC_URL}/404`}
+              component={error404}
+            />
+            <Route component={error404} />
+          </Switch>
+        </PageScrollTop>
+      </BrowserRouter>
+    );
+  }
+}
+
+ReactDOM.render(<Root />, document.getElementById("root"));
+serviceWorker.register();
