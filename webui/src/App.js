1dd55ff2cd63aa68adbf3534b79dcabe1d4d2703
Aaron Shirk <ashirk2013@gmail.com>
2022-04-09 16:56:31 -0400

    adding more heading changes

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,26 +1,49 @@
-import { Spacer, Flex, Heading, IconButton, useColorMode, VStack } from '@chakra-ui/react';
+import {
+  Spacer,
+  Flex,
+  Heading,
+  IconButton,
+  useColorMode,
+  VStack,
+} from '@chakra-ui/react';
 import React from 'react';
-import {FaSun, FaMoon, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaSun, FaMoon, FaGithub, FaLinkedin } from 'react-icons/fa';
 import Header from './components/Header';
 
 function App() {
-  const {colorMode, toggleColorMode} = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === 'dark';
 
   return (
     <VStack p={5}>
       <Flex w="100%">
         <Heading ml={8} size="md" fontWeight="semibold" color="cyan.400">
-          Shirkpy
+          shirkpy
         </Heading>
         <Spacer></Spacer>
-        <IconButton aria-label="LinkedIn Icon" ml={2} icon={<FaLinkedin />} isRound='true' onClick={toggleColorMode}></IconButton>
-        <IconButton aria-labe="Github Icon" ml={2} icon={ <FaGithub /> } isRound='true' onClick={toggleColorMode}></IconButton>
-        <IconButton ml={10} icon={isDark ? <FaSun /> : <FaMoon />} isRound='true' onClick={toggleColorMode}></IconButton>
+        <IconButton
+          aria-label="LinkedIn Icon"
+          ml={2}
+          icon={<FaLinkedin />}
+          isRound="true"
+          onClick={toggleColorMode}
+        ></IconButton>
+        <IconButton
+          aria-labe="Github Icon"
+          ml={2}
+          icon={<FaGithub />}
+          isRound="true"
+          onClick={toggleColorMode}
+        ></IconButton>
+        <IconButton
+          ml={10}
+          icon={isDark ? <FaSun /> : <FaMoon />}
+          isRound="true"
+          onClick={toggleColorMode}
+        ></IconButton>
       </Flex>
       <Header />
     </VStack>
-
   );
 }
 
--- webui/src/components/Header.js
+++ webui/src/components/Header.js
@@ -1,40 +1,72 @@
 import React from 'react';
-import { useColorMode, Stack, Circle, Flex, Box, useMediaQuery, Text, Button } from '@chakra-ui/react';
+import {
+  useColorMode,
+  Stack,
+  Circle,
+  Flex,
+  Box,
+  useMediaQuery,
+  Text,
+  Button,
+  Image,
+} from '@chakra-ui/react';
 
 const Header = () => {
-    const { colorMode } = useColorMode();
-    const isDark = colorMode === "dark";
-    const { isBiggerScreen } = useMediaQuery('(min-width: 600px)');
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === 'dark';
+  const { isBiggerScreen } = useMediaQuery('(min-width: 600px)');
 
-    const OpenLinkedIn = () => {
-        window.open("https://www.linkedin.com/in/aaronshirk/");
-    }
+  const OpenLinkedIn = () => {
+    window.open('https://www.linkedin.com/in/aaronshirk/');
+  };
 
-    return (
-        <Stack direction={["row", "column"]}>
-            <Circle position="absolute" bg="blue.100" opacity="0.1"
-                w="300px" h="300px" alignSelf="flex-end" />
-            <Flex
-                direction={isBiggerScreen ? "row" : "column"}
-                spacing="200px"
-                p={isBiggerScreen ? "32" : "0"}
-                alignSelf="flex-start"
-            >
-                <Box mt={isBiggerScreen ? "0" : "16"} align="flex-start">
-                    <Text fontSize="5xl" fontWeight="semibold">Hi, I am</Text>
-                    <Text
-                        fontSize="7xl"
-                        fontWeight="bold"
-                        bgGradient="linear(to-r, cyan.400, blue.500, purple.600)"
-                        bgClip="text">Aaron Shirk</Text>
-                    <Text color={isDark ? "gray.200": "gray.500"}>
-                        Developer - React, Python, aspiring Data Scientist &#x1F4CA;
-                    </Text>
-                    <Button mt={5} colorScheme={"blue"} onClick={OpenLinkedIn}>Say Hello</Button>
-                </Box>
-            </Flex>
-        </Stack>
-    );
+  return (
+    <Stack>
+      <Circle
+        position="absolute"
+        bg="blue.100"
+        opacity="0.1"
+        w="300px"
+        h="300px"
+        alignSelf="flex-end"
+      />
+      <Flex
+        direction={{ base: 'column', md: 'row' }}
+        p={{ base: '0', md: '32' }}
+        spacing="200px"
+        alignSelf="flex-start"
+      >
+        <Box mt={{ base: '0', md: '16' }} align="flex-start">
+          <Text fontSize="5xl" fontWeight="semibold">
+            Hi, I am
+          </Text>
+          <Text
+            fontSize="7xl"
+            fontWeight="bold"
+            bgGradient="linear(to-r, cyan.400, blue.500, purple.600)"
+            bgClip="text"
+          >
+            Aaron Shirk
+          </Text>
+          <Text color={isDark ? 'gray.200' : 'gray.500'}>
+            Developer - React, Python, aspiring Data Scientist &#x1F4CA;
+          </Text>
+          <Button mt={5} colorScheme={'blue'} onClick={OpenLinkedIn}>
+            Say Hello
+          </Button>
+        </Box>
+        <Image
+          alignSelf={'center'}
+          my={{ base: '12', md: '0' }}
+          backgroundColor="transparent"
+          boxShadow={'lg'}
+          borderRadius="full"
+          boxSize={250}
+          src="https://media-exp1.licdn.com/dms/image/C4D03AQHZezaKSbrtyA/profile-displayphoto-shrink_800_800/0/1561480175086?e=1654732800&v=beta&t=Smh63QLaJifoeTLT_s78fy2002KiKbW6EWb4zqESzBY"
+        />
+      </Flex>
+    </Stack>
+  );
 };
 
 export default Header;
